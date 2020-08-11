2632f05cbdc794592cef8635a504b6ee41313419
Jhon Marreros Guzman <j_marreros@hotmail.com>
2020-08-10 20:06:17 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlBD;
 if ( process.env.NODE_ENV == 'dev' ){
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://admin:holahola@micluster.proo3.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlBD;
